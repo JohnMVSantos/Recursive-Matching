af9256f9706b2e762decbaa0de95be5d88ea8d25
JohnVSantos <137139681+JohnMVSantos@users.noreply.github.com>
2025-02-03 20:50:03 -0700

    Added sphinx docs.

---
+++ docs/conf.py
@@ -0,0 +1,49 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+from subprocess import Popen, PIPE
+
+sys.path.insert(0, os.path.abspath(".."))
+
+def get_version():
+    """
+    Returns project version as string from 'git describe' command.
+    """
+    pipe = Popen('git describe --tags --always', stdout=PIPE, shell=True)
+    return pipe.stdout.read().rstrip().decode('utf-8')
+
+project = 'Recursive Matching'
+copyright = '2025, John Santos'
+author = 'John Santos'
+release = get_version()
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.doctest',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.intersphinx',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/python.rst
@@ -0,0 +1,37 @@
+python package
+==============
+
+Submodules
+----------
+
+python.hungarian module
+-----------------------
+
+.. automodule:: python.hungarian
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+python.recursive module
+-----------------------
+
+.. automodule:: python.recursive
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+python.setup module
+-------------------
+
+.. automodule:: python.setup
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: python
+   :members:
+   :undoc-members:
+   :show-inheritance:
